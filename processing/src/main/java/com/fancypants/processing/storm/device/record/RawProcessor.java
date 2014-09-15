513665a6d5d36f8bf6ec5e11ebbdf9326466a37b
Tim Reidel <reideltj@yahoo.com>
2014-09-14 23:17:35 -0400

    fixes to get processing working

--- processing/src/main/java/com/fancypants/processing/storm/device/record/RawProcessor.java
+++ processing/src/main/java/com/fancypants/processing/storm/device/record/RawProcessor.java
@@ -1,22 +1,22 @@
 package com.fancypants.processing.storm.device.record;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.kinesis.stormspout.KinesisSpout;
-import com.amazonaws.services.kinesis.stormspout.KinesisSpoutConfig;
-import com.fancypants.processing.storm.device.record.scheme.RawRecordScheme;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import storm.trident.TridentTopology;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.services.kinesis.stormspout.KinesisSpout;
+import com.amazonaws.services.kinesis.stormspout.KinesisSpoutConfig;
+import com.fancypants.processing.storm.device.record.auth.CustomAWSCredentialsProvider;
+import com.fancypants.processing.storm.device.record.scheme.RawRecordScheme;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RawProcessor {
 
 	private final static String TOPOLOGY = "raw_records";
 	private final static String TXID = "records";
 	private final static String STREAM = "raw";
-	private final static String ZOOKEEPER_ENDPOINT = "localhost:2181";
+	private final static String ZOOKEEPER_ENDPOINT = "localhost:2000";
 	private final static String ZOOKEEPER_PREFIX = "kinesis_spout";
 
 	private final static ObjectMapper objectMapper = new ObjectMapper();
@@ -30,7 +30,7 @@ public class RawProcessor {
 				.withKinesisRecordScheme(new RawRecordScheme(objectMapper));
 		// create the spout
 		final KinesisSpout spout = new KinesisSpout(config,
-				new DefaultAWSCredentialsProviderChain(),
+				new CustomAWSCredentialsProvider(),
 				new ClientConfiguration());
 		// setup the stream
 		topology.newStream(TXID, spout);
