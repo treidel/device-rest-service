b0cd5a21d266224a48d96028338015bf406bb6bf
Tim Reidel <reideltj@yahoo.com>
2015-05-16 02:32:52 -0400

    fix bug that causes null pointer when querying usage

--- rest/src/main/java/com/fancypants/rest/mapping/PowerConsumptionRecordMapper.java
+++ rest/src/main/java/com/fancypants/rest/mapping/PowerConsumptionRecordMapper.java
@@ -27,11 +27,17 @@ public class PowerConsumptionRecordMapper
 				value.getValue().getEnergy().size());
 		for (Map.Entry<Integer, Float> entry : value.getValue().getEnergy()
 				.entrySet()) {
-			CircuitEntity circuit = value.getKey().getCircuitByIndex(
-					entry.getKey());
-			PowerConsumptionMeasurement measurement = new PowerConsumptionMeasurement(
-					circuit.getName(), entry.getValue());
-			measurements.add(measurement);
+			// get the index
+			int index = entry.getKey();
+			// resolve the circuit
+			CircuitEntity circuit = value.getKey().getCircuitByIndex(index);
+			// if the circuit does not exist it's because the records provide
+			// data for index=max
+			if (null != circuit) {
+				PowerConsumptionMeasurement measurement = new PowerConsumptionMeasurement(
+						circuit.getName(), entry.getValue());
+				measurements.add(measurement);
+			}
 		}
 		// create the record
 		PowerConsumptionRecord record = new PowerConsumptionRecord(value
