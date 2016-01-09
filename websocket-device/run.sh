28599b31bbd4d9862c8913ba6981b520ccb4fe79
Tim Reidel <reideltj@yahoo.com>
2016-01-09 16:10:32 -0500

    remove websocket-device

--- websocket-device/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# get the IP address of the docker interface
-PUBLIC_INTERFACE=`ifconfig docker0 | awk -F"[: ]+" '/inet addr:/ {print $4}'`
-
-# start the container
-docker run -p 7003:7000 -p 8003:8003 -p 8083:8083 -e ZOOKEEPER=$PUBLIC_INTERFACE:2181 -e KAFKA_BROKERS=$PUBLIC_INTERFACE:9092 -e KAFKA_TOPIC=raw -e KEYSTORE_PASSWORD=server -e AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE -e AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY -e AWS_DYNAMODB_ENDPOINT=http://$PUBLIC_INTERFACE:8000 reideltj/websocket-device:DEVELOPER
--- websocket-device/src/main/java/com/fancypants/websocket/device/WebSocketDeviceScanMe.java
+++
@@ -1,5 +0,0 @@
-package com.fancypants.websocket.device;
-
-public interface WebSocketDeviceScanMe {
-
-}
